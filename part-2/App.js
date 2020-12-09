2904fd9c3ac371a5e7669c5dc2293bb3824b61ee
Jose Terrera <joseterrera@yahoo.com>
2020-12-08 18:43:15 -0800

    working on part 3

---
+++ part-2/App.js
@@ -0,0 +1,24 @@
+function App() {
+  return (
+    <div>
+      <Tweet
+        name="Matt Lane"
+        username="mmmaaatttttt"
+        date={new Date().toDateString()}
+        message="This app will disrupt everything!!"
+      />
+      <Tweet
+        name="Jose Terrera"
+        username="joseterrera"
+        date={new Date().toDateString()}
+        message="#Ilovehashtags"
+      />
+      <Tweet
+        name="Bob Hope"
+        username="bobhope"
+        date={new Date().toDateString()}
+        message="Follow me on Twitter!"
+      />
+    </div>
+  );
+}
